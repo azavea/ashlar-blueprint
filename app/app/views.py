c4f67657f8197ceee023df05f3ebd8b3d72f1d86
Jean Cochrane <jean@jeancochrane.com>
2018-06-15 16:39:23 -0400

    Sketch out schema editor component

--- app/app/views.py
+++ app/app/views.py
@@ -1,4 +1,7 @@
+import json
+
 from flask import render_template
+from flask.json import jsonify
 
 from app import application
 
@@ -25,6 +28,14 @@ def type_detail(uuid):
     Display a detail view for a RecordType.
     '''
     return render_template('back/types/detail.html', uuid=uuid)
+
+
+@application.route('/back/schemas/edit/<uuid:uuid>', methods=['GET'])
+def schema_edit(uuid):
+    '''
+    Display an editor for a RecordSchema.
+    '''
+    return render_template('back/schemas/edit.html', uuid=uuid)
 
 
 @application.route('/back/records/', methods=['GET'])
